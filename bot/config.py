b8087012b26a6bcadff6fd1966ac7d1076059b8c
luman233 <luman160@gmail.com>
2025-07-07 12:56:29 +0300

    Update config.py

--- bot/config.py
+++ bot/config.py
@@ -1,13 +1,20 @@
 import os
 
+# Телеграм API ID и HASH — брать с https://my.telegram.org
 API_ID = int(os.getenv("TG_API_ID", 0))
 API_HASH = os.getenv("TG_API_HASH", "")
+# Строка сессии userbot (Pyrogram StringSession)
 SESSION_STRING = os.getenv("TG_SESSION_STRING", "")
 
-# ID целевой группы, куда будут пересылаться сообщения с триггер-словами
+# Список ID групп, которые надо мониторить (обязательно указывать со знаком минус! Пример: -1001234567890)
+SOURCE_GROUP_IDS = [
+    int(x) for x in os.getenv("SOURCE_GROUP_IDS", "-1001234567890,-1009876543210").split(",")
+]
+
+# ID целевой группы (куда будут пересылаться сообщения)
 TARGET_GROUP_ID = int(os.getenv("TARGET_GROUP_ID", 0))
 
-# Список слов/фраз для отслеживания (можно редактировать)
+# Список слов/фраз для фильтрации (чувствительность к регистру отключена)
 TRIGGER_WORDS = [
     "спам",
     "реклама",
